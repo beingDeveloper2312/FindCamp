4129103eddda8678756873bda04be593a0cf356d
beingDeveloper2312 <jaiswalrohit8319@gmail.com>
2023-05-04 17:21:12 +0530

    CHANGED dbUrl

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 const userRoutes = require('./routes/users')
 const helmet = require('helmet');
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
